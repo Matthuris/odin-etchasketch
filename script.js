e7cfe17cc321f15d5cf241c301579be58ce1d533
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-09-29 08:44:14 +0100

    Add grid functionality to createGrid() function

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 const init = function () {
   const button = document.querySelector("button");
   const container = document.querySelector(".container");
-  const newRow = document.createElement("div");
-  container.appendChild(newRow);
-  for (let i = 1; i <= 16; i++) {
-    const newSquare = document.createElement("div");
-    newRow.appendChild(newSquare);
-    newSquare.className = "square";
-  }
-  newRow.className = "row";
-  const row = document.querySelector(".row");
-  for (let j = 1; j < 16; j++) {
-    const cloneRow = row.cloneNode(true);
-    container.appendChild(cloneRow);
-  }
+  const createGrid = function () {
+    const newRow = document.createElement("div");
+    for (let i = 1; i <= 16; i++) {
+      container.appendChild(newRow);
+      const newSquare = document.createElement("div");
+      newRow.appendChild(newSquare);
+      newSquare.className = "square";
+    }
+    newRow.className = "row";
+    const row = document.querySelector(".row");
+    for (let j = 1; j < 16; j++) {
+      const cloneRow = row.cloneNode(true);
+      container.appendChild(cloneRow);
+    }
+  };
+  createGrid();
   container.addEventListener("mouseover", function (e) {
     if (e.target.classList.contains("square")) {
       e.target.classList.add("filled");
