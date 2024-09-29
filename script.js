d435ed2b3a7d88b773cf84c30ddc7aeeb676dac0
Matthuris <175315171+Matthuris@users.noreply.github.com>
2024-09-29 21:41:48 +0100

    Add deleteGrid() which removesChild(grid)

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 const init = function () {
   const container = document.querySelector(".container");
   const button = document.querySelector("button");
-  const grid = document.querySelector(".grid");
 
-  const createGrid = function () {
+  createGrid = function () {
     const newGrid = document.createElement("div");
     container.appendChild(newGrid);
     newGrid.className = "grid";
     const newRow = document.createElement("div");
     newGrid.appendChild(newRow);
     newRow.className = "row";
+
     for (let i = 1; i <= 16; i++) {
       const newSquare = document.createElement("div");
       newRow.appendChild(newSquare);
@@ -21,6 +21,23 @@ const init = function () {
       newGrid.appendChild(cloneRow);
     }
   };
+  getResponse = function () {
+    let response = +prompt("How many squares per side? (1-100");
+    if (response === 0) {
+      return;
+    } else if (Number.isInteger(response) && response > 0 && response <= 100) {
+      deleteGrid();
+      return response;
+    } else {
+      alert("Must be a number between 1 and 100!");
+      getResponse();
+    }
+  };
+
+  deleteGrid = function () {
+    const grid = document.querySelector(".grid");
+    container.removeChild(grid);
+  };
   createGrid();
 
   container.addEventListener("mouseover", function (e) {
@@ -28,17 +45,7 @@ const init = function () {
       e.target.classList.add("filled");
     }
   });
-  getResponse = function () {
-    let response = +prompt("How many squares per side? (1-100");
-    if (response === 0) {
-      return;
-    } else if (Number.isInteger(response) && response > 0 && response <= 100) {
-      return response;
-    } else {
-      alert("Must be a number between 1 and 100!");
-      getResponse();
-    }
-  };
+
   button.addEventListener("click", function () {
     const size = getResponse();
   });
